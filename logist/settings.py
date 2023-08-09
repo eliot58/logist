15d8a83980688ccb766e7a196fdce2accb154794
Aimedin Chorobaev <eliothab58@gmail.com>
2023-08-09 14:11:31 +0600

    settings update

--- logist/settings.py
+++ logist/settings.py
@@ -118,14 +118,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static/'),
-]
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'core/static/'),
+    ]
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_URL = '/media/'  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+    MEDIA_URL = '/media/'  
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+else:
+    MEDIA_ROOT = '/var/www/logist/media'
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = '/var/www/logist/static'
+    STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
